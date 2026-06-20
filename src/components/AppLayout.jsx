412f7b1316bf91303edd2194332dfc3756ad7ec8
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-20 08:20:33 +0600

    feat: create AppLayout component with global navigation, chat widget, and layout scaffolding

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -40,9 +40,7 @@ const AppLayout = () => {
       </div>
 
       <main className="pt-[140px] md:pt-28 pb-20 lg:pb-0 page-content-fade-in">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <Outlet />
-        </div>
+        <Outlet />
       </main>
 
       <RecentlyViewed />
